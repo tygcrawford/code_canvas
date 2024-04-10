29b02b3f5b641add2f3a4ee72ba9dc92bb638c69
Ty Crawford <tygcrawford@gmail.com>
2024-04-10 15:06:57 -0400

    added more options and another raycast mode

--- raycasting/Player.js
+++ raycasting/Player.js
@@ -36,6 +36,43 @@ class Player {
     return rays;
   }
 
+  raycast_flat() {
+    let rays = [];
+    let rightV = p5.Vector.rotate(this.dir, -1*HALF_PI)
+    rightV.mult(params.fov/params.rays)
+    let startPos = p5.Vector.mult(this.dir, params.screen_dist)
+    startPos.add(this.pos)
+    startPos.sub(
+      p5.Vector.mult(rightV, params.rays/2)
+    )
+
+
+    for(let i = 0; i < params.rays; i++) {
+      rays.push(new Ray(startPos.copy(), this.dir.copy()))
+      startPos.add(rightV)
+    } 
+    for (let ray of rays) {
+      let min_ray = null;
+      let min_wall = null;
+      for (let wall of walls) {
+        let current_ray = ray.raycast(wall);
+        if (min_ray == null) {
+          min_ray = current_ray;
+          min_wall = wall;
+        }
+        else if (current_ray != null) {
+          if (ray.pos.dist(current_ray) < ray.pos.dist(min_ray)){
+            min_ray = current_ray;
+            min_wall = wall;
+          }
+        }
+      }
+      ray.cast = min_ray;
+      ray.wall = min_wall;
+    }
+    return rays;
+  }
+
   draw() {
     stroke(255);
     strokeWeight(10);
--- raycasting/main.js
+++ raycasting/main.js
@@ -2,18 +2,35 @@ let player;
 let walls;
 const params = {
   walls: 0,
-  boxes: 2,
-  box_size: 150,
-  fov: 40,
-  rays: 30,
+  boxes: 15,
+  box_size: 5,
+  fov: 60,
+  rays: 400,
+  cast_from: "player",
+  screen_dist: 5,
+  borders: true,
+  distance_correction: true
 };
 
-params.reset = () => {};
+params.reset = () => {
+  walls = [];
+  generateWalls();
+  generateBorders();
+  generateBoxes()
+  player = new Player();
+};
 
 function datSetup() {
   var gui = new dat.GUI({ name: "Raycasting Params" });
   gui.add(params, "fov", 0, 365);
   gui.add(params, "rays", 0, 400);
+  gui.add(params, "screen_dist", 0, 100);
+  gui.add(params, "cast_from", ["screen","player"])
+  gui.add(params, "walls", 0, 20, 1).onChange(params.reset);
+  gui.add(params, "boxes", 0, 40, 1).onChange(params.reset);
+  gui.add(params, "box_size", 0, 200).onChange(params.reset);
+  gui.add(params, "borders").onChange(params.reset);
+  gui.add(params, "distance_correction");
   gui.add(params, "reset");
   gui.close();
 }
@@ -25,6 +42,7 @@ function generateWalls() {
 }
 
 function generateBorders() {
+  if(!params.borders) return;
   let p1, p2;
   p1 = createVector(0,0);
   p2 = createVector(width/2, 0);
@@ -74,15 +92,17 @@ function draw3d(rays) {
     let ray = rays[i]
     if(ray.cast != null) {
       let ray_len = ray.pos.dist(ray.cast);
-      ray_len *= cos(ray.dir.heading() - player.dir.heading())
+      if(params.distance_correction) ray_len *= cos(ray.dir.heading() - player.dir.heading());
       let max_len = sqrt(4 * pow(height,2) + pow(width,2)) / 2;
-      let bar_height = map(ray_len, 0, max_len, height*0.8, height * 0.05);
+      let bar_height = map(ray_len, 0, max_len, height*1, height * 0.05);
+
+      let color_factor = map(ray_len, 0, max_len, 1, 0.4);
 
       if(!ray.horizontalIntersect() && !ray.verticalIntersect()) {
-        fill(0,0,map(ray_len, 0, max_len, 255, 255*0.1));
+        fill(color_factor * 255 * 0.8,0,0);
       }
-      else if (ray.horizontalIntersect()) fill(255, 0, 0)
-      else if (ray.verticalIntersect()) fill(255 * 0.6, 0, 0)
+      else if (ray.horizontalIntersect()) fill(color_factor * 255, 0, 0)
+      else if (ray.verticalIntersect()) fill(color_factor * 255 * 0.6, 0, 0)
       rect(
         width_start + bar_width * i,
         height / 2 - bar_height / 2,
@@ -111,7 +131,9 @@ function draw() {
   drawWalls();
   player.update();
   player.draw();
-  let rays = player.raycast();
+  let rays;
+  if(params.cast_from == "player")rays = player.raycast();
+  else rays = player.raycast_flat();
   for (let ray of rays) {
     ray.draw()
   }
