d8ee640c53da08484452d870923d62cc135805a5
Ty Crawford <tygcrawford@gmail.com>
2024-04-11 15:08:00 -0400

    fisheye finally fixed

--- raycasting/Player.js
+++ raycasting/Player.js
@@ -1,56 +1,99 @@
 class Player {
   constructor() {
-    this.pos = createVector(random(0, width/2), random(0, height));
+    this.pos = createVector(random(0, width / 2), random(0, height));
     this.dir = createVector(1, 1).normalize();
   }
 
-  raycast() {
+  genRaysPlayer() {
     let rays = [];
     let startDir = p5.Vector.rotate(
       this.dir,
       (-1 * PI * 2 * params.fov) / 360 / 2
     );
+
     for (let i = 1; i <= params.rays; i++) {
       rays.push(new Ray(this.pos, startDir.copy()));
       startDir.rotate((PI * 2 * params.fov) / 360 / params.rays);
     }
-    for (let ray of rays) {
-      let min_ray = null;
-      let min_wall = null;
-      for (let wall of walls) {
-        let current_ray = ray.raycast(wall);
-        if (min_ray == null) {
-          min_ray = current_ray;
-          min_wall = wall;
-        }
-        else if (current_ray != null) {
-          if (ray.pos.dist(current_ray) < ray.pos.dist(min_ray)){
-            min_ray = current_ray;
-            min_wall = wall;
-          }
-        }
-      }
-      ray.cast = min_ray;
-      ray.wall = min_wall;
-    }
+
     return rays;
   }
 
-  raycast_flat() {
+  genRaysScreenStraight() {
     let rays = [];
-    let rightV = p5.Vector.rotate(this.dir, -1*HALF_PI)
-    rightV.mult(params.fov/params.rays)
+    let rightV = p5.Vector.rotate(this.dir, HALF_PI)
+    rightV.mult(params.fov / params.rays)
     let startPos = p5.Vector.mult(this.dir, params.screen_dist)
     startPos.add(this.pos)
     startPos.sub(
-      p5.Vector.mult(rightV, params.rays/2)
+      p5.Vector.mult(rightV, params.rays / 2)
     )
 
-
-    for(let i = 0; i < params.rays; i++) {
+    for (let i = 0; i < params.rays; i++) {
       rays.push(new Ray(startPos.copy(), this.dir.copy()))
       startPos.add(rightV)
-    } 
+    }
+
+    return rays;
+  }
+
+  genRaysScreenShifted() {
+    let rays = [];
+    let startDir = p5.Vector.rotate(
+      this.dir,
+      (-1 * PI * 2 * params.fov) / 360 / 2
+    );
+
+    let rightV = p5.Vector.rotate(this.dir, HALF_PI)
+    rightV.mult(params.fov / params.rays)
+    let startPos = p5.Vector.mult(this.dir, params.screen_dist)
+    startPos.add(this.pos)
+    startPos.sub(
+      p5.Vector.mult(rightV, params.rays / 2)
+    )
+
+    for (let i = 0; i < params.rays; i++) {
+      rays.push(new Ray(startPos.copy(), startDir.copy()))
+      startPos.add(rightV)
+      startDir.rotate((PI * 2 * params.fov) / 360 / params.rays);
+    }
+
+    return rays;
+  }
+
+  genRaysCorrect() {
+    // NOTE needs work!!!
+    let rays = [];
+    //let projection_plane_width = tan(radians(params.fov));
+    let projection_plane_width = sin(radians(params.fov) / 2) * params.screen_dist * 2;
+    let projection_plane_distance = params.screen_dist;
+    let cell_width = projection_plane_width / params.rays;
+
+    let rightV = p5.Vector.rotate(this.dir, HALF_PI)
+    rightV.mult(cell_width)
+
+    let rayStart = p5.Vector.mult(this.dir, projection_plane_distance)
+    let half_plane = p5.Vector.mult(rightV, params.rays / 2)
+    rayStart.sub(half_plane)
+    let half_cell = p5.Vector.mult(rightV, 0.5)
+    rayStart.add(half_cell)
+    let rayDir = rayStart.copy()
+
+    for (let cell = 0; cell < params.rays; cell++) {
+      let pointc = p5.Vector.add(this.pos, rayDir)
+      strokeWeight(1)
+      stroke(255, 0, 0)
+      point(pointc)
+      rays.push(new Ray(this.pos.copy(), rayDir.copy().normalize()))
+      rayDir.add(rightV);
+    }
+
+    return rays;
+  }
+
+  raycast(genRays) {
+    let rays = genRays();
+
     for (let ray of rays) {
       let min_ray = null;
       let min_wall = null;
@@ -61,7 +104,7 @@ class Player {
           min_wall = wall;
         }
         else if (current_ray != null) {
-          if (ray.pos.dist(current_ray) < ray.pos.dist(min_ray)){
+          if (ray.pos.dist(current_ray) < ray.pos.dist(min_ray)) {
             min_ray = current_ray;
             min_wall = wall;
           }
--- raycasting/main.js
+++ raycasting/main.js
@@ -6,7 +6,7 @@ const params = {
   box_size: 5,
   fov: 60,
   rays: 400,
-  cast_from: "player",
+  cast_from: "correct",
   screen_dist: 5,
   borders: true,
   distance_correction: true
@@ -21,11 +21,11 @@ params.reset = () => {
 };
 
 function datSetup() {
-  var gui = new dat.GUI({ name: "Raycasting Params" });
-  gui.add(params, "fov", 0, 365);
-  gui.add(params, "rays", 0, 400);
+  var gui = new dat.GUI({ name: "Raycasting Params", width: 300 });
+  gui.add(params, "fov", 0, 170);
+  gui.add(params, "rays", 0, 400, 1);
   gui.add(params, "screen_dist", 0, 100);
-  gui.add(params, "cast_from", ["screen","player"])
+  gui.add(params, "cast_from", ["correct", "mixed", "screen", "player"])
   gui.add(params, "walls", 0, 20, 1).onChange(params.reset);
   gui.add(params, "boxes", 0, 40, 1).onChange(params.reset);
   gui.add(params, "box_size", 0, 200).onChange(params.reset);
@@ -42,38 +42,38 @@ function generateWalls() {
 }
 
 function generateBorders() {
-  if(!params.borders) return;
+  if (!params.borders) return;
   let p1, p2;
-  p1 = createVector(0,0);
-  p2 = createVector(width/2, 0);
+  p1 = createVector(0, 0);
+  p2 = createVector(width / 2, 0);
   walls.push(new Wall(p1, p2));
-  p1 = createVector(0,0);
+  p1 = createVector(0, 0);
   p2 = createVector(0, height);
   walls.push(new Wall(p1, p2));
-  p1 = createVector(0,height);
-  p2 = createVector(width/2, height);
+  p1 = createVector(0, height);
+  p2 = createVector(width / 2, height);
   walls.push(new Wall(p1, p2));
-  p1 = createVector(width/2,0);
-  p2 = createVector(width/2, height);
+  p1 = createVector(width / 2, 0);
+  p2 = createVector(width / 2, height);
   walls.push(new Wall(p1, p2));
 }
 
 function generateBoxes() {
-  for(let i = 0; i < params.boxes; i++) {
-    let x = random(0, width*0.5 - params.box_size)
+  for (let i = 0; i < params.boxes; i++) {
+    let x = random(0, width * 0.5 - params.box_size)
     let y = random(0, height - params.box_size)
     let p1, p2;
     p1 = createVector(x, y)
-    p2 = createVector(x+params.box_size, y)
+    p2 = createVector(x + params.box_size, y)
     walls.push(new Wall(p1, p2));
     p1 = createVector(x, y)
-    p2 = createVector(x, y+params.box_size)
+    p2 = createVector(x, y + params.box_size)
     walls.push(new Wall(p1, p2));
-    p1 = createVector(x, y+params.box_size)
-    p2 = createVector(x+params.box_size, y+params.box_size)
+    p1 = createVector(x, y + params.box_size)
+    p2 = createVector(x + params.box_size, y + params.box_size)
     walls.push(new Wall(p1, p2));
-    p1 = createVector(x+params.box_size, y)
-    p2 = createVector(x+params.box_size, y+params.box_size)
+    p1 = createVector(x + params.box_size, y)
+    p2 = createVector(x + params.box_size, y + params.box_size)
     walls.push(new Wall(p1, p2));
   }
 }
@@ -86,26 +86,26 @@ function drawWalls() {
 
 function draw3d(rays) {
   noStroke();
-  let width_start = width/2;
-  let bar_width = width/2/rays.length;
+  let width_start = width / 2;
+  let bar_width = width / 2 / rays.length;
   for (let i = 0; i < rays.length; i++) {
     let ray = rays[i]
-    if(ray.cast != null) {
+    if (ray.cast != null) {
       let ray_len = ray.pos.dist(ray.cast);
-      if(params.distance_correction) ray_len *= cos(ray.dir.heading() - player.dir.heading());
-      let max_len = sqrt(4 * pow(height,2) + pow(width,2)) / 2;
-      let bar_height = map(ray_len, 0, max_len, height*1, height * 0.05);
-
+      if (params.distance_correction) ray_len *= cos(ray.dir.heading() - player.dir.heading());
+      let max_len = sqrt(4 * pow(height, 2) + pow(width, 2)) / 2;
+      //let bar_height = map(ray_len, 0, max_len, height * 1, height * 0.05);
+      let bar_height = 30000 / ray_len;
       let color_factor = map(ray_len, 0, max_len, 1, 0.4);
 
-      if(!ray.horizontalIntersect() && !ray.verticalIntersect()) {
-        fill(color_factor * 255 * 0.8,0,0);
+      if (!ray.horizontalIntersect() && !ray.verticalIntersect()) {
+        fill(color_factor * 255 * 0.8, 0, 0);
       }
       else if (ray.horizontalIntersect()) fill(color_factor * 255, 0, 0)
       else if (ray.verticalIntersect()) fill(color_factor * 255 * 0.6, 0, 0)
       rect(
         width_start + bar_width * i,
-        height / 2 - bar_height / 2,
+        height / 2 - bar_height * 0.35,
         bar_width + 1,
         bar_height,
       )
@@ -115,7 +115,7 @@ function draw3d(rays) {
 
 function setup() {
   createCanvas(800, 400);
-  
+
   walls = [];
   generateBorders();
   generateWalls();
@@ -131,9 +131,26 @@ function draw() {
   drawWalls();
   player.update();
   player.draw();
-  let rays;
-  if(params.cast_from == "player")rays = player.raycast();
-  else rays = player.raycast_flat();
+
+  let rayGenerator;
+  switch (params.cast_from) {
+    case "player":
+      rayGenerator = player.genRaysPlayer.bind(player);
+      break;
+    case "screen":
+      rayGenerator = player.genRaysScreenStraight.bind(player);
+      break;
+    case "mixed":
+      rayGenerator = player.genRaysScreenShifted.bind(player);
+      break;
+    case "correct":
+      rayGenerator = player.genRaysCorrect.bind(player);
+      break;
+    default:
+      rayGenerator = player.genRaysPlayer.bind(player);
+  }
+  let rays = player.raycast(rayGenerator);
+
   for (let ray of rays) {
     ray.draw()
   }
